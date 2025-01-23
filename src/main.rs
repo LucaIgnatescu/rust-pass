fc39cb7be38ae3d278e304c71f6fd26b8b3e3f74
LucaIgnatescu <lucaignatescu003@gmail.com>
2025-01-23 10:25:46 -0500

    protobuf dummy code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod protos;
+
+use protobuf::Message;
+use protos::example::Example;
+
 fn main() {
-    println!("Hello, world!");
+    let mut example = Example::new();
+    example.set_name(String::from("cheg"));
+    example.set_id(32);
+
+    let serialized = example.write_to_bytes().unwrap();
+    println!("{:?}", serialized);
+
+    let deserialized = Example::parse_from_bytes(&serialized).unwrap();
+    println!("{:?}", deserialized);
 }
